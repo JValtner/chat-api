d6e9160c96e8114585ce5f049fa45845504dd2c0
Josip Valtner <josipvaltner@.com>
2025-11-11 10:49:02 +0100

    test

--- ChatApp/obj/Debug/net8.0/ChatApp.AssemblyInfo.cs
+++ ChatApp/obj/Debug/net8.0/ChatApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChatApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1492bcf49cec63d489eca3b65bcf748e25cd696")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2464e45977de968a4393325aa7f984ca70508860")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
